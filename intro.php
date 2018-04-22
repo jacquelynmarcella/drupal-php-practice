adb03f2c58db1352ff6261a9717f87ab3722bd70
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-04-22 14:23:41 -0700

    Adding some variables and strings to php to render

--- intro.php
+++ intro.php
@@ -1,8 +1,21 @@
+<?php
+
+	$name = 'Jacquelyn';
+	# generating a random number between 1 and 10
+	$num = rand(0, 10);
+
+	# reminder: double quotes here (not single) will allow the $num to print
+	$subhead = "Your number is $num";
+
+?>
+
 <html>
 <head>
 	<title>PHP Test</title>
 </head>
 <body>
-	<?php echo '<h1>Hello World</h1>'; ?>
+	<?php echo 
+		'<h1>' . $name . '</h1><h2>' . $subhead . '</h2>'; 
+	?>
 </body>
 </html>
