760b0eb0740e01cebad9da6b8c2e27bb2be872b9
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-04-22 17:06:03 -0700

    Adding in foreach loops, php arrays' '

--- intro.php
+++ intro.php
@@ -10,22 +10,32 @@
 	$orderAmt = $num1 * QTY;
 
 	if ($orderAmt < 50) {
-		# reminder: double quotes here (not single) will allow the $num to print
+		# reminder: double quotes here (not single) will allow the var to print
 		$subhead = "Amount to order: $orderAmt";
 	} else {
 		$subhead = 'Order size too large';
 	}
 
+	$order_list[] = 'soda';
+	$order_list[] = 'avocado';	
+
 ?>
+
 
 <html>
 <head>
 	<title>PHP Test</title>
 </head>
 <body>
+	
 	<?php echo 
 		'<h1>' . $name . '</h1>' . 
 		'<h2>' . $subhead . '</h2>'; 
+
+		foreach($order_list as $key => $value) {
+			print '<li>' . $value . '</li>';
+		}
 	?>
+
 </body>
 </html>
