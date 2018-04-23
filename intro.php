ec4beeecebf79ce6da0095864d55f791e9c73582
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-04-22 17:47:14 -0700

    Added form for user submission, output in progress

--- intro.php
+++ intro.php
@@ -19,22 +19,45 @@
 	$order_list[] = 'soda';
 	$order_list[] = 'avocado';	
 
+	/**
+	* Prints a string list item
+	* @param array that has strings of order items
+	*/
+	function print_orders ($orders = array('water', 'bread')) {
+	# by setting '$orders = ' we have a default array in case of no params input
+		foreach($orders as $key => $value) {
+			print '<li>' . $value . '</li>';
+		}
+	}
 ?>
-
 
 <html>
 <head>
 	<title>PHP Test</title>
 </head>
 <body>
-	
+
 	<?php echo 
 		'<h1>' . $name . '</h1>' . 
 		'<h2>' . $subhead . '</h2>'; 
+	?>
 
-		foreach($order_list as $key => $value) {
-			print '<li>' . $value . '</li>';
-		}
+	<form name="orderform" method="post" action="intro.php">
+		<p>Category</p>
+		<select name="category">
+			<option value="Food">Food</option>
+			<option value="Drink">Drink</option>
+			<option value="Other">Other</option>
+		</select>
+		<p>Name</p>
+		<input name="name" type="text" id="name" size="50">
+		<input type="submit" name="Submit" value="Submit">
+	</form>
+
+	<h2>Your Order List</h2>
+	<?php
+		# To do: print list of items added to order
+		print_orders($order_list);
 	?>
 
 </body>
